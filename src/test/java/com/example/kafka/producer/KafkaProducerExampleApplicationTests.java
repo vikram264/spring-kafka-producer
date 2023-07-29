7727e04967c0d1a2dcbf7017349feb0dc6da3208
Vikram Reddy <vikram.s.reddy@oracle.com>
2023-07-29 14:41:16 +0530

    initial commit

---
+++ src/test/java/com/example/kafka/producer/KafkaProducerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
